66c59c215bde19827e7ef50aa017940d4cde7b0f
MiM-Home <mortezamim@gmail.com>
2024-08-12 15:56:10 +0330

    [ADD] Create Project Validator

--- Application/Projects/Create/CreateProjectCommandHandler.cs
+++ Application/Projects/Create/CreateProjectCommandHandler.cs
@@ -2,6 +2,7 @@
 using Application.Project.Create;
 using Domain.Projects;
 using Domain.User;
+using FluentValidation;
 using MediatR;
 
 namespace Application.Orders.Create;
@@ -11,19 +12,27 @@ internal sealed class CreateProjectCommandHandler : IRequestHandler<CreateProjec
     private readonly IUserRepository _UserRepository;
     private readonly IProjectRepository _ProjectRepository;
     private readonly IUnitOfWork _unitOfWork;
+    private readonly IValidator<CreateProjectCommand> validator;
 
     public CreateProjectCommandHandler(
         IUserRepository UserRepository,
         IProjectRepository ProjectRepository,
-        IUnitOfWork unitOfWork)
+        IUnitOfWork unitOfWork,
+        IValidator<CreateProjectCommand> validator)
     {
         _UserRepository = UserRepository;
         _ProjectRepository = ProjectRepository;
         _unitOfWork = unitOfWork;
+        this.validator = validator;
     }
 
     public async Task<Guid?> Handle(CreateProjectCommand request, CancellationToken cancellationToken)
     {
+        // Validation
+        var result = await validator.ValidateAsync(request);
+        if (!result.IsValid)
+            throw new Exceptions.ValidationException(result.Errors);
+
         if (await _UserRepository.GetByIdAsync(request.UserId) is null)
         {
             return null;
---
+++ Application/Projects/Create/CreateProjectValidator.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Projects;
+using FluentValidation;
+
+namespace Application.Project.Create;
+
+public sealed class CreateProjectValidator : AbstractValidator<CreateProjectCommand>
+{
+    public CreateProjectValidator(IProjectRepository projectRepository)
+    {
+        RuleFor(x => x.Name).NotEmpty()
+            .WithMessage("Name is required.")
+            .MinimumLength(5)
+            .WithMessage("Name minimum length is 5 characters");
+    }
+}
